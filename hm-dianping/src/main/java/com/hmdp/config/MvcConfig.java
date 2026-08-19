a835865ba9310143b1a9df5a523373d9dbb592f7
csyai0326 <1851279630@qq.com>
2026-08-19 15:57:01 +0800

    HuiGo

--- hm-dianping/src/main/java/com/hmdp/config/MvcConfig.java
+++ hm-dianping/src/main/java/com/hmdp/config/MvcConfig.java
@@ -28,7 +28,6 @@ public class MvcConfig implements WebMvcConfigurer  {
                          "/user/code",
                          "/user/login"
                  ).order(1);
-         registry .addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate ) ).addPathPatterns("/**").order(0) ;
-
+         registry .addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate ) ).addPathPatterns("/**").order(0);
     }
 }
--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,45 @@
 package com.hmdp;
 
+import com.hmdp.utils.RedisIdWorker;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
 
+    @Resource
+    private RedisIdWorker redisIdWorker;
 
+    @Test
+    void testRedisIdWorker() throws InterruptedException {
+        ExecutorService es = Executors.newFixedThreadPool(10, new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                return new Thread(r);
+            }
+        });
+
+        CountDownLatch latch = new CountDownLatch(300);
+
+        Runnable task = () -> {
+            long id = redisIdWorker.nextId("order");
+            System.out.println("id=" + id);
+            latch.countDown();
+        };
+
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+
+        latch.await();
+        long end = System.currentTimeMillis();
+        System.out.println("time = " + (end - begin));
+    }
 }
